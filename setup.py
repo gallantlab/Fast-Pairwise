c95dcbfc7d4da48344b7433629f1e19c0dce0f63
candytaco <t.zhang@berkeley.edu>
2024-07-16 17:03:57 -0700

    `-O2` optimization

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension
 
 PairwiseModule = Extension('Pairwise', sources = ['PairwiseDistances.cpp'],
-						   extra_compile_args = ["-fopenmp", "-lm"],
+						   extra_compile_args = ["-fopenmp", "-lm", "-O2"],
 						   extra_link_args = ["-fopenmp", "-lm"])
 
 setup(name = 'Pairwise', version = '0.1', ext_modules = [PairwiseModule])
