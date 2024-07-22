605c51cc52d2c898c762fb7e25b14b0dd2eb77c9
candytaco <t.zhang@berkeley.edu>
2024-07-22 15:14:58 -0700

    -march=native compile flag

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, Extension
 
 PairwiseModule = Extension('Pairwise', sources = ['PairwiseDistances.cpp', 'PairwiseIndexer.cpp'],
 						   include_dirs = [numpy.get_include()],
-						   extra_compile_args = ["-fopenmp", "-lm", "-O2"],
+						   extra_compile_args = ["-fopenmp", "-lm", "-O2", "-march=native"],
 						   extra_link_args = ["-fopenmp", "-lm"])
 
 setup(name = 'Pairwise', version = '0.1', ext_modules = [PairwiseModule])
